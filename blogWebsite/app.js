b8073a8164f986ff5ed9a72aab9e25dca2b517cc
sudh4800@gmail.com <sudh4800@gmail.com>
2023-10-07 23:42:21 +0530

    BlogWebsite version2 complete

--- blogWebsite/app.js
+++ blogWebsite/app.js
@@ -4,10 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require('lodash');
+const mongoose = require('mongoose');
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+mongoose.connect("mongodb://127.0.0.1:27017/BlogDB");
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,21 +16,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let postsArray = [];
+// const postsArray=[];
+
+// creating new schema for the blogs
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+
+const Blog = mongoose.model("Blog", blogSchema);
 
 app.get("/", (req, res) => {
 
-//  let newPostsArray= postsArray.map((post)=>{
-//   return {
-//     title:post.title,
-//     content:post.content.substring(0,100)+"..."
-//   }
-//  })
+  Blog.find({})
+    .then((blogs) => {
+      // console.log(blogs)
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        postsArray: blogs
+      });
+    })
 
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    postsArray: postsArray
-  });
+
 
 
 })
@@ -52,28 +60,41 @@ app.get("/compose", (req, res) => {
 app.get("/posts/:postName", (req, res) => {
   let postName = req.params.postName;
   postName = lodash.lowerCase(postName);
-  let reqPost=null;
-  postsArray.forEach((post) => {
-    let postTitle = post.title;
-    postTitle = lodash.lowerCase(postTitle);
-    if (postTitle === postName) {
-      const title=lodash.capitalize(post.title);
-      res.render("post",{
-        title:title,
-        content:post.content
+
+
+  Blog.find({})
+    .then((postsArray) => {
+
+      postsArray.forEach((post) => {
+        let postTitle = post.title;
+        postTitle = lodash.lowerCase(postTitle);
+        if (postTitle === postName) {
+          const title = lodash.capitalize(post.title);
+          res.render("post", {
+            title: title,
+            content: post.content
+          })
+        }
       })
-    }
-  })
+    })
+
+
 })
 
 app.post("/compose", (req, res) => {
   let content = req.body.postContent;
   let title = req.body.postTitle;
+
+
+
   let post = {
     title: title,
     content: content
   }
-  postsArray.push(post);
+
+  const blog = new Blog(post);
+  blog.save();
+  // postsArray.push(post);
   res.redirect("/");
 })
 
