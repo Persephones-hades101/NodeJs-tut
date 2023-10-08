938f5a05eaec506a4b4edf95bc6c315077db3d28
sudh4800@gmail.com <sudh4800@gmail.com>
2023-10-08 22:45:16 +0530

    level-1 username and password only

---
+++ Secrets - Starting Code/app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+//import all important modules
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose')
+
+// making connections to mongodb database
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+
+// set middleware and specifying engine for ejs
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", 'ejs');
+
+
+
+// user schema and User Model
+
+const userSchema =new mongoose.Schema({
+  email: String,
+  password: String
+})
+
+const User = mongoose.model("User", userSchema);
+
+
+
+// defning Routes.
+app.get("/", (req, res) => {
+  res.render("home");
+})
+app.get("/login", (req, res) => {
+  res.render("login");
+})
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
+// Registering user in the users collection
+app.post("/register", (req, res) => {
+  const user = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+
+  user.save()
+    .then((result) => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+// searching user in the collection to handle the /login route
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if(foundUser)
+      {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+        else{
+          console.log("Wrong Password");
+          res.redirect("/login");
+        }
+      }
+      else{
+        console.log("Wrong User");
+        res.redirect("/login");
+      }
+      
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+// listening to the port 3000
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+})
